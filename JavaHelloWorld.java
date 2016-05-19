7397445b398529739f09501f7eed0cdb268c2393
chariut <govindachari@gmail.com>
2016-05-19 22:05:37 +0000

    javahelloworld

---
+++ JavaHelloWorld.java
@@ -0,0 +1,8 @@
+public class JavaHelloWorld
+{
+	public static void main (Strng [] args)
+	{
+		System.out.println("Java Hello World");
+		System.out.println("******************************");
+	}
+}
